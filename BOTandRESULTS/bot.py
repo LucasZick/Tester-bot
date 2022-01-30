48df2aab9b577ffe9a3a9e5ff1a5ea9ca49a028e
Lucas Alexandre Zick <91704816+LucasZick@users.noreply.github.com>
2022-01-30 17:07:57 -0300

    Corrected some variables and commented

--- BOTandRESULTS/bot.py
+++ BOTandRESULTS/bot.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 
 time.sleep(10)
-count=2
+count=0
 
 for i in range (15):
     count = count+1
@@ -13,44 +13,44 @@ for i in range (15):
     time.sleep(0.2)
     pyautogui.keyUp('tab')
     time.sleep(0.2)
-    pyautogui.keyUp('alt')
+    pyautogui.keyUp('alt')#go to the terminal
     time.sleep(5)
     pyautogui.press('enter')
     time.sleep(0.5)
-    pyautogui.write('clear')
+    pyautogui.write('clear')#clear to maintain the same pixels of click every try
     time.sleep(0.2)
     pyautogui.press('enter')
     time.sleep(1.5)
-    pyautogui.write('./run-contest LucasZick')
+    pyautogui.write('./run-contest LucasZick')#run the archive
     time.sleep(0.2)
     pyautogui.press('enter')
-    time.sleep(180)
+    time.sleep(180)#wait the finishing of the process
     #baixo(x=76, y=224)
     #cima(x=86, y=133)
     pyautogui.moveTo(x=76,y=224)
     pyautogui.mouseDown(button='left')
     pyautogui.dragTo(x=86,y=133,duration=2)
-    pyautogui.mouseUp(button='left')
-    pyautogui.click(button='right')
+    pyautogui.mouseUp(button='left')#select the results
+    pyautogui.click(button='right')#open the menu of the selection
     time.sleep(0.2)
     pyautogui.press('down')
     time.sleep(0.2)
-    pyautogui.press('enter')
+    pyautogui.press('enter')#select copy
     time.sleep(0.2)
     pyautogui.keyDown('alt')
     time.sleep(0.2)
     pyautogui.press('tab')
     time.sleep(0.2)
-    pyautogui.keyUp('alt')
+    pyautogui.keyUp('alt')#return to the txt file
     time.sleep(1.5)
-    pyautogui.write('RESULTADO '+ str(count))
+    pyautogui.write('RESULTADO '+ str(count))#says whats the position of the result
     for k in range(2):
         time.sleep(0.2)
         pyautogui.press('enter')
         time.sleep(0.2)
     pyautogui.keyDown('ctrl')
     time.sleep(0.2)
-    pyautogui.press('v')
+    pyautogui.press('v')#paste the result
     time.sleep(0.2)
     pyautogui.keyUp('ctrl')
     for j in range(3):
@@ -62,6 +62,6 @@ for i in range (15):
     time.sleep(0.2)
     pyautogui.press('s')
     time.sleep(0.2)
-    pyautogui.keyUp('ctrl')
+    pyautogui.keyUp('ctrl')#save the archive every round
 
-print('FINALIZADO')
+print('Done!')
